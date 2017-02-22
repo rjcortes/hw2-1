2cbe43d8d189a12f6bb269027fa32653f7ad7e30
Rebecca J Cortes <rjc37@afsconnect2.njit.edu>
2017-02-22 14:02:52 -0500

    2

--- display_discount.php
+++ display_discount.php
@@ -16,13 +16,13 @@
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ""; ?></span><br>
+        <span><?php echo "$proddesc"; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ""; ?></span><br>
+        <span><?php echo "$listprice"; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ""; ?></span><br>
+        <span><?php echo "$discperc"; ?></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo ''; ?></span><br>
